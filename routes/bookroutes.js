85bdd03a70ebfe6944dd547c54eda3656b32f3cf
Neeru Rani <neeru.olha@gmail.com>
2020-04-08 19:51:30 +0530

    fixing name

--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 const { authenticate } = require('../middlewares/authenticate')
- 
-const  bookController = require(path.join(__dirname,'../','controllers/bookController'));
+
+const { getAllBooks, getBook, createBook, updateBook, deleteBook } = require("../controllers/bookController");
 //get book list.
-router.get('/', authenticate, bookController.getAllBooks);
+router.get('/', authenticate, getAllBooks);
 //get book detail by id.
-router.get('/:id', authenticate, bookController.getBook);
+router.get('/:id', authenticate, getBook);
 //post a new book.
-router.post('/', authenticate, bookController.createBook);
+router.post('/', authenticate, createBook);
 //update book by id
-router.put('/', authenticate, bookController.updateBook);
+router.put('/', authenticate, updateBook);
 //delete book by id
-router.delete('/:id', authenticate, bookController.deleteBook);
+router.delete('/:id', authenticate, deleteBook);
 
 
 module.exports = router;

17fab5da8b06a81233d39b0e2f56ad3279f53abb
Neeru Rani <neeru.olha@gmail.com>
2020-04-08 19:01:45 +0530

    remove console.log

--- models/mongo.js
+++ models/mongo.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 async function init() {
     try {
        await mongoose.connect('mongodb+srv://neeru:neeru@cluster0-6qdoa.mongodb.net/LibraryManagement?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('connected to mongodb');
+     //   console.log('connected to mongodb');
     } catch (error) {
         console.log("error in mongodb connnection");
         console.log(error);
